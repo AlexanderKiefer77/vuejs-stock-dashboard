7e04812b4ec2b0ac78812fadcfd2d550e5041c1f
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-26 16:04:07 +0100

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-<div id="app">
-</div>
-
+  <BaseCard>
+    <h1>Überschrift</h1>
+    <p>Dies ist ein Beispieltext</p>
+  </BaseCard>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue' // Beispiel für import
+import BaseCard from './components/BaseCard.vue';
 
 export default {
+  
   name: 'App',
   components: {
-    
+    BaseCard
   }
 }
 </script>
 
 <style>
 body {
-  margin: 0;
+  margin: 0;  
 }
 
-#app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+#app {  
+  width: 100vw;
   min-height: 100vh;
-
-
-
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  padding: 100px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+// aktuell keine Props nötig, alles über Slot gesteuert
+</script>
+
+<style scoped>
+.card {
+  background-color: #011F35;
+  border-radius: 16px;
+  padding: 24px 32px;
+}
+</style>
+
